0ff58150256135badd34e422e0645dd659188070
lrnewsom <73595567+lrnewsom@users.noreply.github.com>
2023-11-17 18:23:51 -0600

    Added the 4 aggregating queries as stored procedures, and I added some scripts for data generation.
    The four aggregating queries are stored in separate files as stored procedures. The files beginning with "generate_..." are data generated from generatedata.com

---
+++ back_end/Review Database/Review Database/ProductTypeRanking.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE GetMediaRanking
+    @ProductTypeID INT
+AS
+BEGIN
+    WITH MediaAverageRating AS (
+        SELECT
+            P.ProductID,
+            P.ProductName,
+            AVG(R.Rating) AS AverageScore
+        FROM
+            ReviewSystem.Product AS P
+            LEFT JOIN ReviewSystem.Review AS R ON P.ProductID = R.ProductID
+        WHERE
+            P.ProductTypeID = @ProductTypeID
+        GROUP BY
+            P.ProductID, P.ProductName
+    )
+
+    SELECT
+        ProductID,
+        ProductName,
+        AverageScore,
+        RANK() OVER (ORDER BY AverageScore DESC) AS ReviewRank
+    FROM
+        MediaAverageRating;
+END;
--- back_end/Review Database/Review Database/testing.sql
+++ back_end/Review Database/Review Database/testing.sql
@@ -4,3 +4,9 @@ FROM ReviewSystem.[User];
 SELECT *
 FROM ReviewSystem.Product AS P
 WHERE P.ProductName = 'Logan';
+
+SELECT *
+FROM ReviewSystem.Genre;
+
+SELECT *
+FROM ReviewSystem.Actor;
